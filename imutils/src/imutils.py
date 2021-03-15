afe08baf0716590334093fb920687e0b4be30bfa
ulises.rey <ulises.rey@imp.ac.at>
2021-03-15 19:22:50 +0100

    added makedirctory to the extract_frame function added and improved the add zeros function

--- imutils/src/imutils.py
+++ imutils/src/imutils.py
@@ -23,6 +23,12 @@ def extract_frames(input_image, output_folder, frames_list):
     frames_list: list?
         list of integers, frames that will be extracted
     """
+    if not os.path.exists(output_folder):
+        
+        print('making ', output_folder, ' directory')
+        os.makedirs(output_folder)
+    else: print(output_folder, 'already exists')
+
     
     with tiff.TiffFile(input_image, multifile=False) as tif:
         for i, page in enumerate(tif.pages):
@@ -31,3 +37,37 @@ def extract_frames(input_image, output_folder, frames_list):
                 img=page.asarray()
                 #print(os.path.join(output_folder,'img'+str(i)+'.'+str(file_format)))
                 tiff.imwrite(os.path.join(output_folder,'img'+str(i)+'.tiff'),img)
+                
+def add_zeros_to_filename(path, len_max_number=6):
+    """
+    Change the filename of images from img235.png to img00235.png depending on len_max_number
+    It has a sister function: add_zeros_to_csv
+    Parameters:
+    -----------
+    path: str,
+        Path to the directory with the images
+    len_max_number: int
+        number of digits the number should have, default is 6
+    """
+    #creates numberic regular expression
+    regex_num=re.compile(r'\d+')
+
+    files=os.listdir(path)
+    #this could be improved with a regular expression to catch the numbers.
+    for filename in files:
+        if 'img' not in filename: continue
+        #print(filename)
+        new_filename=filename
+        
+        number=regex_num.search(filename).group(0)
+
+        #get the file exntesion without the do (e.g. 'png') 
+        file_extension=re.split('\.',filename)[-1]
+        
+        #while the number is smaller than the len_max_number
+        while len(number) < len_max_number:
+            #print(re.split('img', filename)[1])
+            number='0'+number
+        #new filename= img string+number+ dot + extension
+        new_filename='img'+number+'.'+file_extension
+        os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
