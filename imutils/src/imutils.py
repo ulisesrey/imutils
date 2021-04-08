0737040d301b3cb7dc6ff60cb251eec5c5736223
ulises.rey <ulises.rey@imp.ac.at>
2021-04-08 10:23:29 +0200

    I adapted this function so that it works again. As input it needs a big tiff file generated with ometiff2bigtiff (my version with pages). Not sure if it works with the output of ometiff2bigtiff from Charlie

--- imutils/src/imutils.py
+++ imutils/src/imutils.py
@@ -120,6 +120,73 @@ def stack2images(input_filename, output_path):
             filename=files[idx]
             tiff.imsave(os.path.join(output_path, filename), img)         
 
+def max_projection_3d(input_filepath, output_filepath, fold_increase=3, nplanes=20):
+
+    """
+    Create a visualization image of a volume, with the 3 max projections possible.
+
+    Parameters:
+    ------------
+    input_filepath: str,
+
+    output_filepath: str,
+
+    fold_increase: int, (it could be float, but code should be tested)
+        Expands the z dimension so that the image has crrect dimensions. Depends on the ratio between xy pixel size and z-step size.
+    nplanes: int,
+
+    """
+    print('creating writer object..')
+    with tiff.TiffWriter(output_filepath, bigtiff=True) as output_tif:
+        print('creating reader object..')
+        with tiff.TiffFile(input_filepath) as tif:
+            print('looping through reader object..')
+            for idx, page in enumerate(tif.pages):#enumerate(tif.series):#enumerate(tif.series[0].pages):
+                if idx%3300==0: print(idx)
+                img=page.asarray()
+                #if it is the first plane of the volume create an empty img_stack shape=(w, h, z)
+                if idx%nplanes==0:
+                    #print('index is ',idx)
+                    img_stack=np.full(shape=(img.shape[0], img.shape[1], nplanes), fill_value=np.nan, dtype=np.uint16)
+
+                #fill the idx plane with the img
+                img_stack[:,:,idx%nplanes]=img
+
+                #if it is the last plane on the volume do max projection on the three axis and concatenate them
+                if idx%nplanes==nplanes-1:
+                    max0 = np.max(img_stack, axis=0)
+                    max1 = np.max(img_stack, axis=1)
+                    max2 = np.max(img_stack, axis=2)
+
+
+
+                    # extends the YZ and XZ max projection for better visualization based on input fold_increase variable
+                    max0= np.repeat(max0, fold_increase, axis=1)
+                    # rotates IM_MAX_2
+                    max0=np.transpose(max0)
+                    max1 = np.repeat(max1, fold_increase, axis=1)
+
+                    #defines corner array
+                    corner_matrix=np.full((fold_increase*img_stack.shape[2],fold_increase*img_stack.shape[2]),100, dtype='uint16')
+
+    #                 # concatenate the different max projections into one image
+    #                 plt.imshow(rep_max1)
+    #                 plt.title('repmax1')
+    #                 plt.show()
+                    vert_conc_1 = cv2.hconcat([max2,max1])
+    #                 plt.imshow(vert_conc_1)
+    #                 plt.title('repmax1,max2')
+    #                 plt.show()
+                    #np.concatenate((max0, rep_max1), axis=0)
+                    vert_conc2 = cv2.hconcat([max0, corner_matrix])#np.concatenate((rep_rot_max0, corner_matrix), axis=0)
+                    final_img = cv2.vconcat([vert_conc_1,vert_conc2])#np.concatenate((vert_conc_1, vert_conc2), axis=1)
+                    #print(final_image.shape)
+    #                 plt.figure(dpi=300)
+    #                 plt.imshow(final_image)
+    #                 plt.show()
+                    #break
+                    #save the 3 max projection image
+                    output_tif.save(final_img, photometric='minisblack')
 
 
 
