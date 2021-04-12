1631e852339efd0d5e1db183b3a21f8341389c1b
ulises.rey <ulises.rey@imp.ac.at>
2021-04-12 14:47:48 +0200

    added parser for the functions (not all yet) in imfunctions.py

---
+++ imutils/src/imutils_parser.py
@@ -0,0 +1,56 @@
+import argparse
+from imutils.src import imfunctions
+
+FUNCTION_MAP = {'tiff2avi' : imfunctions.tiff2avi,
+                'ometiff2bigtiff' : imfunctions.ometiff2bigtiff,
+                'ometiff2bigtiffZ' : imfunctions.ometiff2bigtiffZ,
+                'max_projection_3d': imfunctions.max_projection_3d}
+
+
+# create the top-level parser
+parser = argparse.ArgumentParser(prog='PROG')
+# Just add the subparser, no need to add any other argument.
+# Pass the "dest" argument so that we can figure out which parser was used.
+subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
+
+# create the parser for the "a" command
+parser_a = subparsers.add_parser('tiff2avi', help='tiff2avi help')
+# Keep the names here in sync with the argument names of the FUNCTION_MAP
+parser_a.add_argument("-i", "--tiff_path", required=True, help="path to input tiff file")
+parser_a.add_argument("-o", "--avi_path", required=True, help="path to output avi file")
+parser_a.add_argument("-fourcc", "--fourcc", required=True, help="fourcc compression mode, 0 means no compression")
+parser_a.add_argument("-fps", "--fps", required=True, help="Frames per second")
+
+# create the parser for the "b" command
+parser_b = subparsers.add_parser('ometiff2bigtiff', help='ometiff2bigtiff help')
+parser_b.add_argument("-path", "--path", required=True, help="path to the input folder")
+
+
+# create the parser for the "c" command
+parser_c = subparsers.add_parser('ometiff2bigtiffZ', help='ometiff2bigtiffZ help')
+parser_c.add_argument("-path", "--path", required=True, help="path to the input folder")
+parser_c.add_argument("-output_dir", "--output_dir", required=False, help="path to the output folder")
+parser_c.add_argument("-actually_write", "--actually_write", required=False, help="True if you actually want to write")
+parser_c.add_argument("-num_slices", "--num_slices", required=False, help="Number of slices your Z-Stack should have")
+
+#parser for max_projection_3d
+parser_d= subparsers.add_parser('max_projection_3d', help='max_projection_3d help')
+parser_d.add_argument("-i", "--input_filepath", required=True, help="path to the input image")
+parser_d.add_argument("-o", "--output_filepath", required=True, help="path to the output image")
+parser_d.add_argument("-fold_increase", "--fold_increase", type=int)
+parser_d.add_argument("-nplanes", "--nplanes", type=int)
+
+#create the parser for another function
+
+
+#parse args
+args = parser.parse_args()
+
+# subparser_name has either "tiff2avif" or "ometiff2bigtiff".
+func = FUNCTION_MAP[args.subparser_name]
+# the passed arguments can be taken into a dict like this
+func_args = vars(args)
+# remove "subparser_name" - it's not a valid argument
+del func_args['subparser_name']
+# now call the function with its arguments
+func(**func_args)
