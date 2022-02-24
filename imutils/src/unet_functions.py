a0e2cead101d499cd3dac2ed90ad4d01846c0e9d
Ulises Rey <ulises.rey@imp.ac.at>
2022-02-24 18:20:04 +0100

    uncommented reshape and resize, created img_original_shape variable

--- imutils/src/unet_functions.py
+++ imutils/src/unet_functions.py
@@ -2,16 +2,7 @@
 import cv2
 import tifffile as tiff
 
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-from natsort import natsorted
-import re
-
-import pandas as pd
-import csv
-
-#unet model is here (too)
+# unet model is here (too)
 from imutils.src.model import *
 
 
@@ -24,7 +15,7 @@ def unet_segmentation(img, model):
     :param model: unet model
     :return: segmented_img
     """
-
+    img_original_shape=img.shape
     # run U-Net network:
     img = cv2.resize(img, (256, 256))
     img = np.reshape(img, img.shape + (1,))
@@ -33,9 +24,9 @@ def unet_segmentation(img, model):
     img = img / 255
     results = model.predict(img)
     # reshape results
-    #results_reshaped = results.reshape((800,800))
+    results_reshaped = results.reshape(img_original_shape)
     # resize results
-    #results_reshaped = cv2.resize(results_reshaped, (w, h))
+    results_reshaped = cv2.resize(results_reshaped, (w, h))
     # multiply it by 255
     segmented_img = results * 255
 
@@ -50,6 +41,7 @@ def unet_segmentation_stack(input_filepath, output_filepath, weights_path):
 
     #load model and weights
     model=unet()
+    print('loading weights..')
     model.load_weights(weights_path)
 
     with tiff.TiffFile(input_filepath, multifile=False) as tif,\
