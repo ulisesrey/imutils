b64c54ede524dc4c7cc332d47f19ac2681279465
Ulises Rey <ulises.rey@imp.ac.at>
2022-03-10 11:43:05 +0100

    added functions saveResult and labelVisualize

--- imutils/src/unet_functions.py
+++ imutils/src/unet_functions.py
@@ -68,6 +68,20 @@ def testGenerator(test_path, target_size = (256,256),flag_multi_class = False, a
         img = np.reshape(img,(1,)+img.shape)
         yield img
 
+def labelVisualize(num_class,color_dict,img):
+    """this function is duplicated from unet-master/data.py"""
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255
+
+def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
+    """this function is duplicated from unet-master/data.py"""
+    for i,item in enumerate(npyfile):
+        img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
+        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img_as_ubyte(img))
+
 
 def predict_test_images(test_path, weights_path, save_path):
     """
