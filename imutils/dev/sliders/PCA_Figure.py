e670eebd6b409e3907836c98287b1c8d57888185
Ulises Rey <ulises.rey@imp.ac.at>
2022-03-15 15:29:28 +0100

    renamed file

--- imutils/dev/sliders/PCA_Figure.py
+++ imutils/dev/sliders/PCA_Figure.py
@@ -7,13 +7,13 @@ import pandas as pd
 #wbfm
 path='/Users/ulises.rey/local_data/Oriana_worm3/worm3_Oriana_PCA_5Components.csv'
 #skeleton behaviour
-path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/worm3/wbfm_worm3_PCA.csv'
+#path='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/worm3/wbfm_worm3_PCA.csv'
 df=pd.read_csv(path)#, header=None)
 
 #path to motor state annotation
 path_motor_state='/Users/ulises.rey/local_data/Oriana_worm3/w3_state.csv'
 #skeleton behaviour
-path_motor_state='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/worm3/beh_annotation_16_subsamples_timeseries_with_long_reversals.csv'
+#path_motor_state='/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/worm3/beh_annotation_16_subsamples_timeseries_with_long_reversals.csv'
 motor_state_df=pd.read_csv(path_motor_state)
 
 avg_win=10#167
@@ -38,6 +38,6 @@ plt.subplots_adjust(left=0.25, bottom=0.25)
 ax1 = fig.add_subplot(1, 1, 1, projection='3d')
 
 ax1.scatter(x, y, z, lw=0.5, c=motor_state_df['state'].map(color_dict), s=3)
-ax1.set_axis_off()
+#ax1.set_axis_off()
 
 plt.show()
