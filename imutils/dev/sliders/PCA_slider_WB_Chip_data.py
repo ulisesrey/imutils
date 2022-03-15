3cec0298aa46f1cb96923990ddb84a32e0970946
Ulises Rey <ulises.rey@imp.ac.at>
2022-03-15 10:29:52 +0100

    added averaging and color coding

--- imutils/dev/sliders/PCA_slider_WB_Chip_data.py
+++ imutils/dev/sliders/PCA_slider_WB_Chip_data.py
@@ -6,16 +6,25 @@ from sklearn.decomposition import PCA
 import tifffile as tiff
 import zarr
 
+#path to PC
 path='/Users/ulises.rey/local_data/Oriana_worm3/w3_PCA.csv'
 df=pd.read_csv(path, header=None)
-avg_win=167#167
-x=df.iloc[:,[10]].values.flatten()
-y=df.iloc[:,[20]].values.flatten()
-z=df.iloc[:,[30]].values.flatten()
+
+#path to motor state annotation
+path_motor_state='/Users/ulises.rey/local_data/Oriana_worm3/w3_state.csv'
+motor_state_df=pd.read_csv(path_motor_state)
+
+avg_win=10#167
+
+
+x=df.iloc[:,[0]].rolling(window=avg_win, center=True).mean().values.flatten()
+y=df.iloc[:,[1]].rolling(window=avg_win, center=True).mean().values.flatten()
+z=df.iloc[:,[2]].rolling(window=avg_win, center=True).mean().values.flatten()
 # x=df.iloc[:,[0]].rolling(window=avg_win, center=True).mean().values.flatten()
 # y=df.iloc[:,[1]].rolling(window=avg_win, center=True).mean().values.flatten()
 # z=df.iloc[:,[2]].rolling(window=avg_win, center=True).mean().values.flatten()
 print(type(z))
+print(len(z))
 # Create the figure and the line that we will manipulate
 fig = plt.figure(figsize=plt.figaspect(0.5), dpi=200)
 #fig, ax = plt.subplots()
@@ -23,8 +32,8 @@ fig = plt.figure(figsize=plt.figaspect(0.5), dpi=200)
 ax1 = fig.add_subplot(1, 1, 1, projection='3d')
 
 
-ax1.plot(x, y, z, lw=0.5, color='grey')
-ax1.plot(x,y,z, lw=2)
+ax1.scatter(x, y, z, lw=0.5, c=motor_state_df['state_color'], s=3)
+ax1.plot(x,y,z, lw=0.5, c='grey', alpha=0.5)
 
 
 plt.show()
