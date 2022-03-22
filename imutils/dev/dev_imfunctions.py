567a2855ca4f81c8a933fb500efb0733ec9c5400
Ulises Rey <ulises.rey@imp.ac.at>
2022-03-22 18:06:39 +0100

    script to write text on top of tiff image, based on behavioural annotation

--- imutils/dev/dev_imfunctions.py
+++ imutils/dev/dev_imfunctions.py
@@ -1,28 +1,53 @@
 import cv2
 import tifffile as tiff
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def add_text(img, text, position, font, color):
-    """
-    position (x,y) tuple
-    color, (R,G,B) tuple from  o to 254)
-    """
-    #img is your image in below line
-    img = cv2.putText(img, text, position, font, 1, color, 2)
-    #Display the image
-    return img
 
-path='/Users/ulises.rey/local_data/MondaySeminar3/2021-03-04_16-17-30_worm3_ZIM2051-channel-0-bigtiff_new_inv_MED_bg.btf kept stack_contrasted_inverted.tif'
+
+path='/Users/ulises.rey/local_data/MondaySeminar3/2021-03-04_16-17-30_worm3_ZIM2051-channel-0-bigtiff_new_inv_MED_bg.btf kept stack_contrasted_inverted_90degrees_left.tif'
+output_path='/Users/ulises.rey/local_data/MondaySeminar3/2021-03-04_16-17-30_worm3_ZIM2051-channel-0-bigtiff_new_inv_MED_bg.btf kept stack_contrasted_inverted_90degrees_left_annotated.tif'
+
+path_motor_state='/Users/ulises.rey/local_data/worm3_wbfm_PCA/beh_annotation_16_subsamples_timeseries_with_long_reversals.csv'
+# #'/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/worm3/beh_annotation_16_subsamples_timeseries_with_long_reversals.csv'
+motor_state_df=pd.read_csv(path_motor_state)
+#subsample motorstate
+#motor_state_df=motor_state_df.loc[::2,:]
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-position = (300,100)
-tif = tiff.TiffFile(path)
+position = (300,165)
 
-img = tif.pages[0].asarray()
-plt.imshow(img)
-plt.show()
+color_dict = {'forward': (0,0,255),
+              'reversal': (255,0,0),
+              'sustained reversal': (255,0,0),
+              'ventral turn': (255,140,0),
+              'dorsal turn': (255,140,0)}
 
-img= add_text(img, 'Forward', position, font, (255,255,255))
-plt.imshow(img)
-plt.show()
+text_dict = {'forward': 'Forward',
+              'reversal': 'Reversal',
+              'sustained reversal': 'Reversal',
+              'ventral turn': 'Turn',
+              'dorsal turn': 'Turn'}
+
+with tiff.TiffFile(path) as tif, tiff.TiffWriter(output_path) as tif_writer:
+    for idx, page in enumerate(tif.pages):
+        img = page.asarray()
+        # i had to multiply the index because the behaviour was annotated on a subsampled image
+        idx=idx*2
+
+        # convert the img to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        #print(motor_state_df.loc[idx,'state'])
+        # Try statement in case idx is outside of the dataframe range
+        try:
+            text = text_dict[motor_state_df.loc[idx,'state']]
+            color = color_dict[motor_state_df.loc[idx,'state']]
+            #print('text is :', text, 'color is :', color)
+            # add text
+            img = cv2.putText(img, text, position, font, 1.5, color, 4)
+
+        except: print('idx not in image')
+        # write
+        tif_writer.write(img)# not required: photometric='rgb')
+        #break
