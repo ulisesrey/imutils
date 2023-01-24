c9b4680ea5d9124326e3422a1f5ebc1bbfd659b0
Ulises Rey <ulises.rey@univie.ac.at>
2023-01-24 19:04:09 +0100

    added documentation on how to do it

--- imutils/src/napari_centerline_viewer.py
+++ imutils/src/napari_centerline_viewer.py
@@ -4,6 +4,10 @@ import napari
 from skimage.morphology import skeletonize
 
 
+# I think following these instructions it should be doable to read the csv and draw the points on top:
+#https://napari.org/stable/howtos/layers/points.html
+#Section : Setting edge or face color with a colormap¶
+
 #There is a notebook like this in the epifluorescence package
 img_path_list=["/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221127/data/ZIM2165_Gcamp7b_worm1/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BH/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BHbigtiff_AVG_background_subtracted_normalised_unet_segmented_weights_5358068_1_mask_coil_segmented_weights_5848336_0_mask.btf"]
 
@@ -19,7 +23,6 @@ for img_path in img_path_list:
 
     #image_layer
     viewer.add_image(img, blending='additive')
-    viewer.add_image(skel, blending='additive')
 
 napari.run()
 
