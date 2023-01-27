d9c4a1986945d0ed5d3f2f06900d942d879cb35d
Ulises Rey <ulises.rey@univie.ac.at>
2023-01-27 15:31:15 +0100

    almost working version, added average, added confidence as color
    need to figure correct cmap limits to display blue and red, right now only showing blue for + and - values.

--- imutils/src/napari_centerline_viewer.py
+++ imutils/src/napari_centerline_viewer.py
@@ -3,6 +3,7 @@ import zarr
 import napari
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # I think following these instructions it should be doable to read the csv and draw the points on top:
@@ -17,6 +18,9 @@ import numpy as np
 merged_spline_data_path = "/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221127/data/ZIM2165_Gcamp7b_worm1/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BH/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BHbigtiff_skeleton_merged_spline_data.csv"
 spline_df = pd.read_csv(merged_spline_data_path, header=[0,1], index_col=0)
 
+spline_df = spline_df.rolling(48, center=True, min_periods=24).mean()
+
+
 # df.loc[0]['0'][["x", "y"]]
 #x = spline_df.xs(('x',), level=('coords',), axis=1)#spline_df.loc[frame][:,'x'].values
 #y = spline_df.xs(('y',), level=('coords',), axis=1)#spline_df.loc[frame][:,'y'].valuesy
@@ -28,20 +32,7 @@ points = (
     .to_numpy()
 )
 
-#melt (I dont think I need this)
-#x = x.melt(ignore_index = False)['value']
-#y = y.melt(ignore_index = False)['value']
 
-#time vector
-#t_vec = np.expand_dims(np.array(list(spline_df.index), dtype=int), axis=1)
-
-#points = np.stack((x,y), axis=2)
-print(points.shape)
-
-
-
-#points = points.reshape(points.shape[0], 100, 2)
-print(points.shape)
 
 #reshape data to be 1D
 #points = points.reshape(-1) #points, newshape=)
@@ -54,12 +45,20 @@ print(points.shape)
 #points = points[1:, :]
 #print(points.shape)
 
+#old methd
+
+#good method
 point_properties = {
-    'confidence': spline_df.xs(('k',), level=('coords',), axis=1),
+    'confidence': spline_df.rename_axis("index").stack("segment").reset_index()[["index", "k"]].to_numpy()[:,1]
 }
 
+#this brings nans to 0, # Fill NaNs with 0 so that the color can be plotted, otherwise error
+# point_properties = np.nan_to_num(point_properties)
+
+
+
 #There is a notebook like this in the epifluorescence package
-img_path_list=["/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221127/data/ZIM2165_Gcamp7b_worm1/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BH/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BHbigtiff_AVG_background_subtracted_normalised_unet_segmented_weights_5358068_1_mask_coil_segmented.btf"]
+img_path_list=["/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221127/data/ZIM2165_Gcamp7b_worm1/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BH/2022-11-27_15-14_ZIM2165_worm1_GC7b_Ch0-BHbigtiff_AVG_background_subtracted_normalised_unet_segmented_weights_5358068_1_mask_coil_segmented_mask.btf"]
 
 # with napari.gui_qt() as app:
 viewer = napari.Viewer()
@@ -72,25 +71,14 @@ for img_path in img_path_list:
     viewer.add_image(img, blending='additive')
 
     #points layer
-    points_layer = viewer.add_points(points)
-    # points_layer = viewer.add_points(
-    #     data=points,
-    #     properties=point_properties,
-    #     face_color='confidence',
-    #     face_colormap='bwr',
-    # )
-
-# def set_pts(pts_layer, values_table, step):
-#     # step is a 4D coordinate with the current slider position for each dim
-#     column = step[0]  # grab the leading ("time") coordinate
-#     pts_layer.features['value'] = np.asarray(values_table[:, column])
-#     pts_layer.face_color = spline_df.loc[column][:,'k'].values  #column or step? # force features refresh
-#
-#
-# viewer.dims.events.current_step.connect(
-#         lambda event: set_pts(points_layer, points, event.value)
-#         )
-
+    #points_layer = viewer.add_points(points)
+    points_layer = viewer.add_points(
+        data=points,
+        properties=point_properties,
+        face_color='confidence',
+        face_colormap='bwr',
+        size=10
+    )
 
 napari.run()
 
