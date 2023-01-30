7aa86a23a4bee8468aa6d890297fb883e83b542a
Ulises Rey <ulises.rey@univie.ac.at>
2023-01-30 11:43:33 +0100

    added blending, and glob

--- imutils/src/napari_2channels_viewer.py
+++ imutils/src/napari_2channels_viewer.py
@@ -7,13 +7,22 @@ import matplotlib.pyplot as plt
 import zarr
 import napari
 
+#There is a notebook like this in the epifluorescence package
 
-project_path = '/Volumes/scratch/neurobiology/zimmer/ulises/active_sensing/epifluorescence_recordings/25x/20220707_25x/data/ZIM1661_worm9'
+project_path = '/Volumes/scratch/neurobiology/zimmer/active_sensing/zim06/20230111/data/worm1'
+    #'/Volumes/scratch/neurobiology/zimmer/ulises/active_sensing/epifluorescence_recordings/20220520/data/ZIM1661_worm5/'
 
-red_img_path=glob.glob(os.path.join(project_path, '*/*worm*Ch0bigtiff.btf'))[0]
-green_img_path=glob.glob(os.path.join(project_path, '*/*worm*Ch1bigtiff.btf'))[0]
+#beh_img_path=glob.glob(os.path.join(project_path, '*/*worm*Ch0-BHbigtiff.btf'))[0]
+red_img_path=glob.glob(os.path.join(project_path, '*Ch0/*bigtiff*masked.btf'))[0]
+green_img_path=glob.glob(os.path.join(project_path, '*Ch1/*bigtiff*masked.btf'))[0]
 
+#two images:
+#red_img_path = "/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221210/data/ZIM2165_Gcamp7b_worm8/2022-12-10_20-13_ZIM2165_worm8_Ch0-BH/2022-12-10_20-13_ZIM2165_worm8_Ch0-BHbigtiff_AVG_background_subtracted_normalised_worm_with_centerline.btf"
+#green_img_path = "/Volumes/scratch/neurobiology/zimmer/ulises/wbfm/20221210/data/ZIM2165_Gcamp7b_worm8/2022-12-10_20-13_ZIM2165_worm8_Ch0-BH/2022-12-10_20-13_ZIM2165_worm8_Ch0-BHbigtiff_AVG_background_subtracted_normalised_unet_segmented_weights_5358068_1_mask_coil_segmented_weights_5374562_0_mask.btf"
 print(red_img_path, green_img_path)
+
+#beh_img=tiff.imread(beh_img_path, aszarr=True)
+#beh_img = zarr.open(beh_img, mode='r')
 
 green_img=tiff.imread(green_img_path, aszarr=True)
 green_img = zarr.open(green_img, mode='r')
@@ -25,7 +34,12 @@ red_img = zarr.open(red_img, mode='r')
 #with napari.gui_qt() as app:
 viewer = napari.Viewer()
 
-red_layer = viewer.add_image(red_img)
-#red_layer.colormap = 'red'
-green_layer = viewer.add_image(green_img)
+red_layer = viewer.add_image(red_img, blending='additive')
+red_layer.colormap = 'red'
+
+green_layer = viewer.add_image(green_img, blending='additive')
+green_layer.colormap = 'green'
+#beh_layer=viewer.add_image(beh_img)
+
 napari.run()
+
