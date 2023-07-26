802aabfbb2ef54860dbeb9d0e981ef04d07b9648
Ulises Rey <ulises.rey@univie.ac.at>
2023-07-26 17:38:37 +0200

    commented main

--- imutils/src/measure_signal_from_DLC_annotation.py
+++ imutils/src/measure_signal_from_DLC_annotation.py
@@ -43,31 +43,31 @@ def extract_bodypart_coords_from_DLC(h5, bodypart):
     return bodypart_coordinates
 
 #if __name__ == "__main__":
-
-# inputs
-
-projects= glob.glob("/Volumes/scratch/neurobiology/zimmer/active_sensing/zim06/zim2391/test_dlc_pipeline/20230515/data/worm*/")
-print(projects)
-
-for project_path in projects:
-    print(project_path)
-    red_path = glob.glob(os.path.join(project_path, '*Ch0/'))[0]
-    green_path = glob.glob(os.path.join(project_path, '*Ch1/'))[0]
-    print("red path is: ", red_path)
-    print("green path is: ", green_path)
-    h5 = glob.glob(os.path.join(red_path, "*mobnet*.h5"))[0]
-    print("h5 file is: ", h5)
-    red_image_path = os.path.join(red_path, "raw_stack_cropped.btf")
-    green_image_path = os.path.join(green_path, "raw_stack_cropped.btf")
-
-    # This should be loaded as dictionary with the bodypart as key and the coordinates as values
-    bodypart = "soma" #'soma'
-    dist = 2
-
-    coords = extract_bodypart_coords_from_DLC(h5, bodypart)
-
-    df = measure_signal_from_coords(red_image_path, coords, dist)
-    df.to_csv(os.path.join(red_path, "raw_stack_background_subtracted_masked_measurements.csv"))
-    df = measure_signal_from_coords(green_image_path, coords, dist)
-    df.to_csv(os.path.join(green_path, "raw_stack_background_subtracted_masked_measurements.csv"))
-print("done")
+#
+# # inputs
+#
+# projects= glob.glob("/Volumes/scratch/neurobiology/zimmer/active_sensing/zim06/zim2391/test_dlc_pipeline/20230515/data/worm*/")
+# print(projects)
+#
+# for project_path in projects:
+#     print(project_path)
+#     red_path = glob.glob(os.path.join(project_path, '*Ch0/'))[0]
+#     green_path = glob.glob(os.path.join(project_path, '*Ch1/'))[0]
+#     print("red path is: ", red_path)
+#     print("green path is: ", green_path)
+#     h5 = glob.glob(os.path.join(red_path, "*mobnet*.h5"))[0]
+#     print("h5 file is: ", h5)
+#     red_image_path = os.path.join(red_path, "raw_stack_cropped.btf")
+#     green_image_path = os.path.join(green_path, "raw_stack_cropped.btf")
+#
+#     # This should be loaded as dictionary with the bodypart as key and the coordinates as values
+#     bodypart = "soma" #'soma'
+#     dist = 2
+#
+#     coords = extract_bodypart_coords_from_DLC(h5, bodypart)
+#
+#     df = measure_signal_from_coords(red_image_path, coords, dist)
+#     df.to_csv(os.path.join(red_path, "raw_stack_background_subtracted_masked_measurements.csv"))
+#     df = measure_signal_from_coords(green_image_path, coords, dist)
+#     df.to_csv(os.path.join(green_path, "raw_stack_background_subtracted_masked_measurements.csv"))
+# print("done")
