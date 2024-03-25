c675c23b8e6be35104bd4ac77339695888805a59
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-03-25 13:56:20 +0100

    Update DLC_analyze_videos.py

--- imutils/src/DLC_analyze_videos.py
+++ imutils/src/DLC_analyze_videos.py
@@ -3,13 +3,13 @@ import os
 os.environ["DLClight"]="True"
 import deeplabcut
 import sys
+import time  # Import the time module
 
 
 def main(arg_list=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--path_config_file", required=True, help="path to config file")
     parser.add_argument("--videofile_path", required=True, help="path to the videofile")
-    #args = vars(ap.parse_args())
     args = parser.parse_args(arg_list)
 
     print(args)
@@ -23,6 +23,11 @@ def main(arg_list=None):
     #although this code on the jupyternotebook works, i had an error while using the .py file. It went away with gputouse=None (although I was in a GPU env)
     deeplabcut.analyze_videos(path_config_file, videofile_path, videotype=VideoType, gputouse=None, save_as_csv=True)
     deeplabcut.filterpredictions(path_config_file, videofile_path, videotype=VideoType, save_as_csv=True, windowlength=3)
+
+    # Wait for 1 minute (60 seconds) for server file system:
+    print("Waiting for the server file system to be ready to create labeled videos")
+    time.sleep(60)  # Wait for 60 seconds
+    print("Resuming create labeled videos...")
 
     deeplabcut.create_labeled_video(path_config_file, videofile_path, videotype=VideoType, save_frames=False)
 
