9e904b676cf6cb76f629b9298fa494e686cae343
LukasMicroscopy <83710538+LukasMicroscopy@users.noreply.github.com>
2024-04-09 11:33:49 +0200

    tiny fixes, Thanks Charlie!

--- MicroscopeDataReader/MicroscopeDataReader.py
+++ MicroscopeDataReader/MicroscopeDataReader.py
@@ -12,6 +12,12 @@ class MicroscopeDataReader:
         Reads data from a microscope data file. The file can be a NDTiff file or a MMStack file.
         If the file is a NDTiff file, the data is read using the ndtiff package.
         
+        If a folder is passed, specific naming conventions are assumed.
+        For ndtiff (if force_tifffile is True):
+            first_tiff_file = self.directory_path.name + '_NDTiffStack.tif'
+        and for MMStack:
+            first_tiff_file = self.directory_path.name + '_MMStack.ome.tif'
+        
         Args:
             path (Path or str): Path to the file or directory containing the data
             force_tifffile (bool, optional): If True, the file is read with tifffile. Defaults to False.
@@ -45,61 +51,64 @@ class MicroscopeDataReader:
     def _check_directory_path(self, directory_path: Union[Path,str]) -> None:
          # Check if file_path is of type pathlib.Path
         if isinstance(directory_path, Path):
-            self.directroy_path = directory_path
+            self.directory_path = directory_path
         elif isinstance(directory_path, str):
-            self.directroy_path = Path(directory_path)
+            self.directory_path = Path(directory_path)
         else:
             self.logger.error("file_path is not of type pathlib.Path or str")
             raise TypeError("file_path is not of type pathlib.Path or str")
          # Check if file or path exists:
-        if not self.directroy_path.exists():
-            self.logger.error(f"File {self.directroy_path} does not exist")
-            raise FileNotFoundError(f"File {self.directroy_path} does not exist")
+        if not self.directory_path.exists():
+            self.logger.error(f"File {self.directory_path} does not exist")
+            raise FileNotFoundError(f"File {self.directory_path} does not exist")
         # check if self.filename is a file or a folder:
-        if not self.directroy_path.is_dir():
-            self.first_tiff_file = self.directroy_path.name
-            self.directroy_path = self.directroy_path.parent
+        if not self.directory_path.is_dir():
+            self.first_tiff_file = self.directory_path.name
+            self.directory_path = self.directory_path.parent
     
-    def _open_dataset(self) -> None: 
-        self.logger.info(f"Reading Dataset from: {self.directroy_path}")
-        if (self.directroy_path/'NDTiff.index').exists():
-            self.logger.info(f"Found NDTiff.index file in {self.directroy_path}")
+    def _open_dataset(self) -> None:
+        """
+        Opens the dataset. Also contains logic for finding first file if a directory is passed.
+        """
+        self.logger.info(f"Reading Dataset from: {self.directory_path}")
+        if (self.directory_path/'NDTiff.index').exists():
+            self.logger.info(f"Found NDTiff.index file in {self.directory_path}")
             self.is_mm_stack = False
             self.is_ndtiff = True
             if self._force_tifffile:
-                self.logger.info(f"Force reading with tiffile: {self.directroy_path}")
+                self.logger.info(f"Force reading with tiffile: {self.directory_path}")
                 if self.first_tiff_file is None:
-                    self.first_tiff_file = self.directroy_path.name + '_NDTiffStack.tif'
+                    self.first_tiff_file = self.directory_path.name + '_NDTiffStack.tif'
                 self._read_tifffile()
                 return
             self._read_ndtiff()
             return
         if self.first_tiff_file is None:
-            self.first_tiff_file = self.directroy_path.name + '_MMStack.ome.tif'
-        if (self.directroy_path / self.first_tiff_file).exists():
-            self.logger.info(f"Found {self.directroy_path}/{self.first_tiff_file} file in {self.directroy_path}")
+            self.first_tiff_file = self.directory_path.name + '_MMStack.ome.tif'
+        if (self.directory_path / self.first_tiff_file).exists():
+            self.logger.info(f"Found {self.directory_path}/{self.first_tiff_file} file in {self.directory_path}")
             self.is_mm_stack = True
             self.is_ndtiff = False
             self._read_tifffile()
             return
         else:
-            self.logger.error(f"Could not find {self.directory_path}/{self.first_tiff_file} file in {self.directroy_path}")
-            raise FileNotFoundError(f"Could not find {self.directory_path}/{self.first_tiff_file} file in {self.directroy_path}")
+            self.logger.error(f"Could not find {self.directory_path}/{self.first_tiff_file} file in {self.directory_path}")
+            raise FileNotFoundError(f"Could not find {self.directory_path}/{self.first_tiff_file} file in {self.directory_path}")
             
     def _read_ndtiff(self):
-        self.logger.info(f"Reading data from {self.directroy_path} as ndtiff file")
+        self.logger.info(f"Reading data from {self.directory_path} as ndtiff file")
         from ndtiff import Dataset
-        self._data_store = Dataset(self.directroy_path)
+        self._data_store = Dataset(self.directory_path)
         self._dask_array = self._data_store.as_array()
         self.logger.info(f"Data store: {self._data_store}")
         self.logger.info(f"dask array dimensions [position?,t,channel?,z,y,x]: {self._dask_array.shape}")
         
     def _read_tifffile(self):
-        filepath = self.directroy_path / self.first_tiff_file
+        filepath = self.directory_path / self.first_tiff_file
         if not filepath.exists():
-            self.logger.error(f"Could not find {self.first_tiff_file} file in {self.directroy_path}")
-            raise FileNotFoundError(f"Could not find {self.first_tiff_file} file in {self.directroy_path}")
-        self.logger.info(f"Reading data from {self.directroy_path} as MMStack file")
+            self.logger.error(f"Could not find {self.first_tiff_file} file in {self.directory_path}")
+            raise FileNotFoundError(f"Could not find {self.first_tiff_file} file in {self.directory_path}")
+        self.logger.info(f"Reading data from {self.directory_path} as MMStack file")
         import tifffile as tff
         if version.parse(tff.__version__) < version.parse(self._tifffile_version):
             self.logger.error(f"tifffile version {tff.__version__} is not supported. Please update to version {self._tifffile_version} or higher")
@@ -139,7 +148,7 @@ class MicroscopeDataReader:
                 # add +1 to all elements of org_axis_position if they are >= 0 (not -1) to take into account the added axis
                 org_axis_position[i+1:] = [x+1 if x >= 0 else x for x in org_axis_position[i+1:]]
             elif not org_axis_position[i] == i:
-                self.logger.debug(f"swapp axis {org_axis_position[i]} with {i}")
+                self.logger.debug(f"swap axis {org_axis_position[i]} with {i}")
                 dask_array = dask.array.moveaxis(dask_array, org_axis_position[i], i)
                 # also flip axis in org_axis_position
                 old_axis = org_axis_position[i]
@@ -153,7 +162,7 @@ class MicroscopeDataReader:
         
     def read_image(self, position: int = 0, time: int = 0, channel: int = 0, z: int = 0) -> np.array:
         """
-        Reads an single y,x image from the data set. The image is selected by the position, time, channel and z values.
+        Reads a single y,x image from the data set. The image is selected by the position, time, channel and z values.
         
         Args:
             position (int, optional): position. Defaults to 0.
@@ -190,7 +199,7 @@ class MicroscopeDataReader:
     
     def open_in_napari(self):
         """
-        Opens the data set in napari
+        Opens the data set in napari using view_image. This assumes the image is floats (not ints, like segmentation)
         """
         import napari
         viewer = napari.view_image(self._dask_array, multiscale=False, rgb=False, axis_labels=self.axis_order)
