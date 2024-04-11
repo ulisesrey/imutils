e3ba5699901e75fda3eba24d49be9f595829b384
LukasMicroscopy <83710538+LukasMicroscopy@users.noreply.github.com>
2024-04-11 22:22:09 +0200

    stupid setup

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Zimmer-lab/imutils",
-    packages=(setuptools.find_packages()), #+['MicroscopeDataReader']
-    py_modules=['MicroscopeDataReader/MicroscopeDataReader'],
-    #package_dir={"":"},
+    packages=(setuptools.find_packages()),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
