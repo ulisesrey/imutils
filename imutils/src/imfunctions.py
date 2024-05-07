41db1ae9d00c42d30fdec09668dcf6b7ecb54313
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-05-07 11:59:20 +0200

    added option to binaryze on nob substracted background for populatio pipeline

--- imutils/src/imfunctions.py
+++ imutils/src/imfunctions.py
@@ -311,7 +311,7 @@ def stack_subsample(stack_input_filepath, stack_output_filepath, range):
 
 
 def make_contour_based_binary(stack_input_filepath, stack_output_filepath, median_blur, threshold,
-                              max_value, contour_size, tolerance, inner_contour_area_to_fill, gaussian_blur=0):
+                              max_value, contour_size, tolerance, inner_contour_area_to_fill, gaussian_blur=0, substract_background=1):
 
     """
     Produce a binary image based on contour and inner contour sizes, by calling draw_some_contours()
@@ -336,6 +336,10 @@ def make_contour_based_binary(stack_input_filepath, stack_output_filepath, media
             for i, page in enumerate(tif.pages):
                 # loads the first frame
                 img = page.asarray()
+
+                if substract_background != 1:
+                    img = 255 - img
+
                 # median Blur
                 if gaussian_blur != 0:
                     img = cv2.GaussianBlur(img, (gaussian_blur, gaussian_blur), 0)
