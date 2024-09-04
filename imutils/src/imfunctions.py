789fa954157cc3d4bfda8e9b911233fa7507a1d0
BenjaminSchaar <122351773+BenjaminSchaar@users.noreply.github.com>
2024-09-04 12:44:41 +0200

    Update imfunctions.py

--- imutils/src/imfunctions.py
+++ imutils/src/imfunctions.py
@@ -54,15 +54,14 @@ def tiff2avi(tiff_path, avi_path, fourcc, fps):
     tiff_path = os.path.abspath(tiff_path)
     print('Path absolute:', tiff_path)
 
-    # Check if the input path is a directory or a BTF file
-    try:
+    if os.path.isdir(tiff_path):
+        # Initialize for directory
         reader_obj = MicroscopeDataReader(tiff_path)
-    except:
-        if tiff_path.lower().endswith('.btf'):
-            # Initialize for BTF file
-            reader_obj = MicroscopeDataReader(tiff_path, as_raw_tiff=True, raw_tiff_num_slices=1)
-        else:
-            raise ValueError("Invalid input file path. Please provide a directory or a .btf file.")
+    elif tiff_path.lower().endswith('.btf'):
+        # Initialize for BTF file
+        reader_obj = MicroscopeDataReader(tiff_path, as_raw_tiff=True, raw_tiff_num_slices=1)
+    else:
+        raise ValueError("Invalid input file path. Please provide a directory or a .btf file.")
 
     tif = da.squeeze(reader_obj.dask_array)
     frame_size_unknown_len = tif[0].shape
